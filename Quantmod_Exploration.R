e14c82a703803d86db7f2b02690593f6204ae2ed
Frank Chang <fchang7@gatech.edu>
2018-02-19 23:26:54 -0600

    Turned OptionChain into function

--- Quantmod_Exploration.R
+++ Quantmod_Exploration.R
@@ -6,12 +6,14 @@ require(quantmod)
 
 
 # Get Stock Info ----------------------------------------------------------
-getSymbols(c("SPY"), from=as.Date("16-01-01", format="%y-%m-%d"))
+getSymbols(c("SPY",
+             "BHF",
+             "COP"), from=as.Date("16-01-01", format="%y-%m-%d"))
 
 
 # Chart Series ------------------------------------------------------------
 x11()
-chartSeries(SPY)
+chartSeries(COP)
 addBBands()
 addMACD()
 addRSI()
@@ -19,29 +21,49 @@ addExpiry()
 
 
 # Options Info ------------------------------------------------------------
-# Getting historical options prices
-SPY[options.expiry(SPY),]
+# Turning it into a function
 
-# Get All Option Chains
-SPYOptionChain <- getOptionChain("SPY", "2018")
+OptionChain <- function(stockTickerDF){
+  # get ticker name
+  stockTicker <- deparse(substitute(stockTickerDF))
+  
+  # Getting historical options prices
+  HistoricalOptionPriceDF <- stockTickerDF[options.expiry(stockTickerDF),]
+  
+  # Get All Option Chains
+  theOptionChain <- getOptionChain(stockTicker, "2018")
+  
+  # Options Chain - Calls In the Money (NextOptions Expiry Date)
+  CallInTheMoney <- theOptionChain[[1]]$calls[theOptionChain[[1]]$calls$Strike <= getQuote(stockTicker)$Last,]
+  
+  # Options Chain - Puts In The Money (NextOptions Expiry Date)
+  PutInTheMoney <- theOptionChain[[1]]$puts[theOptionChain[[1]]$puts$Strike >= getQuote(stockTicker)$Last,]
+  
+  # Quick General Market Sentiment Based on Volumne of Options
+  # Total vol sum of Call Option strike price out of money / Total vol sum of Put Option strike price out of money
+  # 1 = Neutral
+  # > 1 = Bullish
+  # < 1 = Bearish
+  VolBull <- sum(theOptionChain[[1]]$calls[theOptionChain[[1]]$calls$Strike >= getQuote(stockTicker)$Last,]$Vol)
+  VolPut <- sum(theOptionChain[[1]]$puts[theOptionChain[[1]]$puts$Strike <= getQuote(stockTicker)$Last,]$Vol)
+  BullToPutRatio <- VolBull/VolPut
+  
+  # Options Chain - Calls Out of the Money (NextOptions Expiry Date)
+  CallOutOfTheMoney <- theOptionChain[[1]]$calls[theOptionChain[[1]]$calls$Strike >= getQuote(stockTicker)$Last,]
+  
+  # Options Chain - Puts Out of The Money (NextOptions Expiry Date)
+  PutOutOfTheMoney <- theOptionChain[[1]]$puts[theOptionChain[[1]]$puts$Strike <= getQuote(stockTicker)$Last,]
 
-# Options Chain - Calls In the Money (NextOptions Expiry Date)
-SPYOptionChain[[1]]$calls[SPYOptionChain[[1]]$calls$Strike <= getQuote("SPY")$Last,]
-
-# Options Chain - Puts In The Money (NextOptions Expiry Date)
-SPYOptionChain[[1]]$puts[SPYOptionChain[[1]]$puts$Strike >= getQuote("SPY")$Last,]
-
-# Quick General Market Sentiment Based on Volumne of Options
-# Closest Call Option strike price out of money / Closet Put Option strike price out of money
-# 1 = Neutral
-# > 1 = Bullish
-# < 1 = Bearish
-VolBull <- SPYOptionChain[[1]]$calls[SPYOptionChain[[1]]$calls$Strike >= getQuote("SPY")$Last,][1,]$Vol
-VolPut <- tail(SPYOptionChain[[1]]$puts[SPYOptionChain[[1]]$puts$Strike <= getQuote("SPY")$Last,], n=1)$Vol
-BullToPutRatio <- VolBull/VolPut
-
-# Options Chain - Calls Out of the Money (NextOptions Expiry Date)
-SPYOptionChain[[1]]$calls[SPYOptionChain[[1]]$calls$Strike >= getQuote("SPY")$Last,]
-
-# Options Chain - Puts Out of The Money (NextOptions Expiry Date)
-SPYOptionChain[[1]]$puts[SPYOptionChain[[1]]$puts$Strike <= getQuote("SPY")$Last,]
+  # Return the List
+  theList <- list("HistoricalOptionPriceDF" = HistoricalOptionPriceDF,
+                  "theOptionChain" = theOptionChain,
+                  "CallInTheMoney" = CallInTheMoney,
+                  "PutInTheMoney" = PutInTheMoney,
+                  "VolBull" = VolBull,
+                  "VolPut" = VolPut,
+                  "BullToPutRatio" = BullToPutRatio,
+                  "CallOutOfTheMoney" = CallOutOfTheMoney,
+                  "PutOutOfTheMoney" = PutOutOfTheMoney)
+  
+  return(theList)
+}
